77806b9840868e7d5d3112af5d567ee9e450a1d3
Alex Johnson <me@alex-johnson.net>
2016-04-02 15:10:40 -0400

    fixed facebook login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,7 +24,28 @@ angular.module('starter.controllers', [])
 .controller('AccountCtrl', function($scope, Auth) {
   $scope.settings = {
     login: function() {
-      
+    Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
+    // User successfully logged in
+    }).catch(function(error) {
+      if (error.code === "TRANSPORT_UNAVAILABLE") {
+        Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+          // User successfully logged in. We can log to the console
+          // since we’re using a popup here
+          console.log(authData);
+        });
+      } else {
+        // Another error occurred
+        console.log(error);
+      }
+      Auth.$onAuth(function(authData) {
+        if (authData === null) {
+          console.log("Not logged in yet");
+        } else {
+            console.log("Logged in as", authData.uid);
+        }
+          $scope.authData = authData;
+        });
+      });
     },
     logout: function() {
       Auth.$unauth();
