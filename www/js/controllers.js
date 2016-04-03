d0e6e2ea1ea8bfc84dca838128ad4c782577a619
Lynn <lluong9@gmail.com>
2016-04-03 07:17:00 -0400

    changed our last fuckers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,34 @@
 angular.module('starter.controllers', [])
 
+
+.controller('LoginCtrl', function($scope, Auth) {
+  $scope.login = function() {
+    Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
+      // User successfully logged in
+    }).catch(function(error) {
+      if (error.code === "TRANSPORT_UNAVAILABLE") {
+        Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+          // User successfully logged in. We can log to the console
+          // since we’re using a popup here
+          console.log(authData);
+        });
+      } else {
+        // Another error occurred
+        console.log(error);
+      }
+      Auth.$onAuth(function(authData) {
+        if (authData === null) {
+          console.log("Not logged in yet");
+        } else {
+          console.log("Logged in as", authData.uid);
+        }
+        $scope.authData = authData;
+      });
+    });
+  }
+})
+
+
 .controller('HomeCtrl', function($scope, Auth) {
   $scope.login = function() {
     Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
@@ -27,6 +56,23 @@ angular.module('starter.controllers', [])
   }
 })
 
+.controller('StaringInterfaceCtrl', function($scope, Camera) {
+
+$scope.opponentPhoto = function() {
+                           console.log('Getting camera');
+                           Camera.getPicture({
+                             quality: 75,
+                             targetWidth: 320,
+                             targetHeight: 320,
+                             saveToPhotoAlbum: false
+                           }).then(function(imageURI) {
+                             console.log(imageURI);
+                             $scope.lastPhoto = imageURI;
+                           }, function(err) {
+                             console.err(err);
+                           });
+    }
+})
 .controller('LeaderboardCtrl', function($scope) {})
 
 .controller('MeCtrl', function($scope, Auth) {
--- www/js/services.js
+++ www/js/services.js
@@ -4,4 +4,20 @@ angular.module('starter.services', ["ionic", "firebase"])
   var usersRef = new Firebase("https://looksd.firebaseio.com/users");
   return $firebaseAuth(usersRef);
 })
+.factory('Camera', ['$q', function($q) {
 
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
